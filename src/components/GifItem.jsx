ee3ee0ac8d151fff2902aaa62815c3884bd16a38
Andhre <mizandhre@gmail.com>
2024-07-23 19:59:50 -0500

    correcion de estilos card

--- src/components/GifItem.jsx
+++ src/components/GifItem.jsx
@@ -1,7 +1,9 @@
 export const GifItem = ({ titulo, url, id }) => {
 	return (
-		<div className='border-4 border-teal-500 shadow-white rounded-[10px] hover:scale-105 hover:cursor-pointer '>
-			<img src={url} alt={titulo} className='w-full h-full object-cover  aspect-square' />
+		<div
+			className='border-4 border-teal-500  rounded-[10px] hover:scale-105 hover:cursor-pointer  overflow-hidden 
+		hover:shadow-xl hover:shadow-teal-600/50'>
+			<img src={url} alt={titulo} className='w-full h-full object-cover  aspect-square ' />
 
 			{/* <p >{titulo}</p> */}
 		</div>
